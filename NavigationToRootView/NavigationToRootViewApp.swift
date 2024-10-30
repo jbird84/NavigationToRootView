eb59fb02090f704b855b3a910d1b0500b546c46a
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-10-29 19:17:09 -0500

    Initial Commit

---
+++ NavigationToRootView/NavigationToRootViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationToRootViewApp.swift
+//  NavigationToRootView
+//
+//  Created by Kinney Kare on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationToRootViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationToRootViewTests/NavigationToRootViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationToRootViewTests.swift
+//  NavigationToRootViewTests
+//
+//  Created by Kinney Kare on 10/29/24.
+//
+
+import Testing
+@testable import NavigationToRootView
+
+struct NavigationToRootViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
